8d0be78e69c4302ee69392acd705f9aa6f544aaf
Herdy <54350341+herdyhardiyant@users.noreply.github.com>
2023-12-06 13:43:57 +0700

    dto login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,21 @@
-import { Controller, HttpCode, Post, Body, HttpStatus, UseGuards, Request, Get } from '@nestjs/common';
+import { Controller, HttpCode, Post, Body, HttpStatus, UseGuards, Request, Get, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from './auth.guard';
+import { SignInDto } from './sign-in.dto';
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) {}
 
     @HttpCode(HttpStatus.OK)
     @Post('login')
-    signIn(@Body() signInDto: Record<string, any>) {
-        //TODO use ValidationPipe
+    signIn(@Body() signInDto: SignInDto) {
         return this.authService.signIn(signInDto.username, signInDto.password);
     }
 
     @UseGuards(AuthGuard)
     @Get('profile')
-    getProfile(@Request() req){
+    getProfile(@Request() req)
+    {
         return req.user;
     }
 
