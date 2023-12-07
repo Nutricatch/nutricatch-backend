0c29320990a264b48ac55a85b9b01a05fb306bc9
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2023-12-07 20:45:25 +0700

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FoodsModule } from './foods/foods.module';
 import { AuthModule } from './auth/auth.module';
-
 import { UsersModule } from './users/users.module';
 import { BlogsModule } from './blogs/blogs.module';
 
 @Module({
   imports: [FoodsModule, AuthModule, UsersModule, BlogsModule],
-
-import { ConfigModule } from '@nestjs/config';
-
-@Module({
-  imports: [FoodsModule, AuthModule, ConfigModule.forRoot() ],
-  providers: [],
 })
 export class AppModule {}
