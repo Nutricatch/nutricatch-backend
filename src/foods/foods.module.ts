f5cff3453e6c96d0db6586965382f41d8cbb9006
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2023-12-07 12:35:42 +0700

    add scv with colum

--- src/foods/foods.module.ts
+++ src/foods/foods.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { FoodsController } from './foods.controller';
 import { FoodsService } from './foods.service';
