ecbd0874cee20dc4f869eeec0dd507106fd5140b
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2023-12-08 13:36:32 +0700

    ✨ add blog id

--- src/blogs/blogs.service.ts
+++ src/blogs/blogs.service.ts
@@ -1,18 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import * as fs from 'fs';
 
 @Injectable()
 export class BlogsService {
-  async getBlogs(): Promise<any> {
-    const blogFilePath = 'public/blogs.json';
-    try {
-      const rawData = fs.readFileSync(blogFilePath, 'utf8');
-      const blogsData = JSON.parse(rawData);
-      return blogsData;
-    } catch (error) {
-      throw new Error(
-        `Terjadi kesalahan saat membaca file JSON: ${error.message}`,
-      );
+  private readonly blogFilePath = 'public/blogs.json';
+
+  private getBlogsData(): any[] {
+    const rawData = fs.readFileSync(this.blogFilePath, 'utf8');
+    const parsedData = JSON.parse(rawData);
+    return parsedData;
+  }
+
+  getBlogs(): any[] {
+    return this.getBlogsData();
+  }
+
+  getBlogById(id: number): any {
+    const blogs = this.getBlogsData();
+    const blog = blogs.find((blog) => blog.id === id);
+
+    if (!blog) {
+      throw new NotFoundException(`Blog in Id '${id}' not found`);
     }
+
+    return blog;
   }
 }
