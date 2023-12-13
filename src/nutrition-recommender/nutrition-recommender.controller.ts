9a5628ccbbaf81bd0fda8fbfbc5116834b578934
Herdy <54350341+herdyhardiyant@users.noreply.github.com>
2023-12-13 17:26:43 +0700

    daily calories recommendation

--- src/nutrition-recommender/nutrition-recommender.controller.ts
+++ src/nutrition-recommender/nutrition-recommender.controller.ts
@@ -2,10 +2,14 @@ import { Controller, Get, Request } from '@nestjs/common';
 import { UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { AuthGuard } from 'src/auth/auth.guard';
+import { NutritionRecommenderService } from './nutrition-recommender.service';
+
 @Controller('nutrition-recommender')
 export class NutritionRecommenderController {
 
-    constructor(){}
+    constructor(
+        private nutritionRecommenderService: NutritionRecommenderService
+    ){}
 
     //TODO
     // Get daily recommended calories and nutrition
@@ -15,7 +19,7 @@ export class NutritionRecommenderController {
     @Get('daily-recomended-nutrition')
     async getDailyRecommendation(@Request() req){
         const userId: number = req.user.userId;
-        
+        return await this.nutritionRecommenderService.getDailyCaloriesRecommendation(userId)
     }
 
 
--- src/nutrition-recommender/nutrition-recommender.service.ts
+++ src/nutrition-recommender/nutrition-recommender.service.ts
@@ -6,16 +6,48 @@ export class NutritionRecommenderService {
 
     constructor(
         private userHealthService: UserHealthService
-    ){}
-
-    async getDailyCaloriesRecommendation(userId: number){
+    ) { }
+    
+    // We use the Harris-Benedict equation
+    async getDailyCaloriesRecommendation(userId: number) {
         const userHealth = await this.userHealthService.userHealth(userId)
-        if(!userHealth.gender || !userHealth.weight || !userHealth.age || !userHealth.height){
+        if (!userHealth.gender || !userHealth.weight || !userHealth.age || !userHealth.height) {
             throw new NotFoundException("User data is not enough to calculate recommendation")
         }
 
-        
+        const men_bmr = 88.362 
+        + (9.247 * userHealth.weight) 
+        + (4.799 * userHealth.height) 
+        - (5.677 * userHealth.age)
 
+        const women_bmr = 447.593 
+        + (9.247 * userHealth.weight)
+        +(3.098 * userHealth.height)
+        - (4.330 * userHealth.age)
+
+        const usedConst = userHealth.gender === "MALE" ? men_bmr : women_bmr
+        
+        if(userHealth.activityLevel === "LIGHTLY"){
+            return usedConst * 1.2
+        }
+
+        if(userHealth.activityLevel === "SEDENTARY"){
+            return usedConst * 1.375
+        }
+
+        if(userHealth.activityLevel === "MODERATELY"){
+            return usedConst * 1.55
+        }
+
+        if(userHealth.activityLevel === "VERY_ACTIVE"){
+            return usedConst * 1.725
+        }
+
+        if(userHealth.activityLevel === "EXTREMELY_ACTIVE"){
+            return usedConst * 1.9
+        }
+
+        
 
     }
 
