dfde7212ddb31a0d8797eb70c7f4b9fa2424f1cb
Herdy <54350341+herdyhardiyant@users.noreply.github.com>
2023-12-14 17:47:24 +0700

    macros recommender

--- src/nutrition-recommender/nutrition-recommender.service.ts
+++ src/nutrition-recommender/nutrition-recommender.service.ts
@@ -7,13 +7,33 @@ export class NutritionRecommenderService {
     constructor(
         private userHealthService: UserHealthService
     ) { }
+
+    async getAllDailyRecommendation(userId:number){
+        const calories = await this.getDailyCaloriesRecommendation(userId)
+        const protein = this.getDailyProtein(calories)
+        const fats = this.getDailyFats(calories)
+        const carbohydrates = this.getDailyCarbohydrates(calories)
+        return {calories: calories.toFixed(), protein, fats, carbohydrates}
+    }
+
+    getDailyProtein(userDailyCalores:number,  ratio=0.3){
+        const protein = userDailyCalores * ratio / 4
+        return protein.toFixed()
+    }
+
+    getDailyCarbohydrates(userDailyCalores:number,  ratio=0.4){
+        const carbohydrates = userDailyCalores * ratio / 4
+        return carbohydrates.toFixed()
+    }
+
+    getDailyFats(userDailyCalores:number, ratio=0.3){
+        const fats = userDailyCalores * ratio / 9
+        return fats.toFixed()
+    }
     
     // We use the Harris-Benedict equation
     async getDailyCaloriesRecommendation(userId: number) {
-        const userHealth = await this.userHealthService.userHealth(userId)
-        if (!userHealth.gender || !userHealth.weight || !userHealth.age || !userHealth.height) {
-            throw new NotFoundException("User data is not enough to calculate recommendation")
-        }
+        const userHealth = await this.getUserHealth(userId)
 
         const men_bmr = 88.362 
         + (9.247 * userHealth.weight) 
@@ -27,15 +47,11 @@ export class NutritionRecommenderService {
 
         const usedConst = userHealth.gender === "MALE" ? men_bmr : women_bmr
         
-        if(userHealth.activityLevel === "LIGHTLY"){
-            return usedConst * 1.2
-        }
-
         if(userHealth.activityLevel === "SEDENTARY"){
             return usedConst * 1.375
         }
 
-        if(userHealth.activityLevel === "MODERATELY"){
+        if(userHealth.activityLevel === "MODERATELY_ACTIVE"){
             return usedConst * 1.55
         }
 
@@ -43,12 +59,13 @@ export class NutritionRecommenderService {
             return usedConst * 1.725
         }
 
-        if(userHealth.activityLevel === "EXTREMELY_ACTIVE"){
-            return usedConst * 1.9
-        }
-
-        
-
     }
 
+    private async getUserHealth(userId: number){
+        const userHealth = await this.userHealthService.userHealth(userId)
+        if (!userHealth.gender || !userHealth.weight || !userHealth.age || !userHealth.height) {
+            throw new NotFoundException("User data is not enough to calculate recommendation")
+        }
+        return userHealth
+    }
 }
