d64313f51c0d6db23d06c82047eb47bec0c44670
Herdy <54350341+herdyhardiyant@users.noreply.github.com>
2023-12-15 17:28:06 +0700

    add tags

--- src/foods/foods.controller.ts
+++ src/foods/foods.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { FoodsService } from './foods.service';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('foods')
 export class FoodsController {
   constructor(private readonly foodService: FoodsService) {}
 
+  @ApiTags('Foods')
   @Get()
   async getFoods() {
     const foodData = await this.foodService.getFoods();
